e37e2b157d85b665e55164ad5e92d3d1fe046286
pramdf042 <84129088+pramdf042@users.noreply.github.com>
2025-02-23 12:53:02 +0700

    Update skripsi.py

--- skripsi.py
+++ skripsi.py
@@ -120,11 +120,11 @@ with st.container():
         tfidf_transformer = joblib.load("tfidf_transformer.pkl")
         #Frekuensi Kata
         X_count = count_vectorizer.fit_transform(data['processed_text'])
-        df_count = pd.DataFrame(X_count.toarray(),columns=count_vectorizer.get_feature_names_out())
         # #TF-IDF
-        # X_tfidf = tfidf_transformer.fit_transform(X_count)
+        X_tfidf = tfidf_transformer.fit_transform(X_count)
+        df_tfidf = pd.DataFrame(X_tfidf.toarray(),columns=count_vectorizer.get_feature_names_out())
         st.write("TF-IDF")
-        st.write(df_count.head(10))
+        st.write(df_tfidf.head(10))
     if selected == "Implementation":
         # Load the CountVectorizer and TfidfTransformer
         count_vectorizer = joblib.load("count_vectorizer.pkl")
